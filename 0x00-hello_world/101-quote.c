86420a518413a2c446535c6f4d5967ff62da3b32
douniiia98 <donyabachar6@gmail.com>
2023-08-10 17:33:44 -0700

    dounia

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - prints the following string "with proper grammar, but the outcome is a piece of art,"
- * 
+ * main - prints a string 
  * Return: zero if working correctly
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return (1);
 }
