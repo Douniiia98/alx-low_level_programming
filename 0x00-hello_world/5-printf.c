0f3cbbaba8988e3f5ad83a882639bbcdb5db5608
douniiia98 <donyabachar6@gmail.com>
2023-08-10 17:37:04 -0700

    dounia

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the following string "with proper grammar, but the outcome is a piece of art,"
- * 
+ * main - prints the following string "with proper grammar, but the ... "
+ *
  * Return: zero if working correctly
  */
 int main(void)
