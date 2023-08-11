3678eeddd5c7c02ea2eab45fc1c9473b5a6b7e5b
douniiia98 <donyabachar6@gmail.com>
2023-08-10 17:38:05 -0700

    dounia

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the following string "Programming is like building a multilingual puzzle
+ * main - prints the following string "Programming is like building ..
  * Return: zero if working correctly
  */
 
