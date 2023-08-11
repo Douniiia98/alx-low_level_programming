5acac7cdd2208b9d7a4a3b5f5ae33ef5b211f527
douniiia98 <donyabachar6@gmail.com>
2023-08-10 17:34:28 -0700

    dounia

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 /**
- * main - prints a string 
+ * main - prints a string
  * Return: zero if working correctly
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
 	return (1);
 }
