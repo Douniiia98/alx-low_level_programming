83c159021e5a7ce16fddfdb941f45e823dd1b3d9
douniiia98 <donyabachar6@gmail.com>
2023-08-17 03:07:17 +0300

    dounia

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,46 +4,38 @@
  * main - prints the first 50 Fibonacci numbers
  * Return: 0 if success
  */
-
 int main(void)
 {
-	unsigned long Fn_l, Fn_r, F1_l = 0;
-	unsigned long F2_l = 0, F1_r = 1, F2_r = 2;
-	int i;
+	unsigned long int i, j, k, j_1, j_2, k_1, k_2;
 
-	for (i = 0; i < 98; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (i < 2)
-		{
-			Fn_l = 0;
-			Fn_r = F1_r + (F1_r * i);
-		}
-		else
-		{
-			Fn_l = F1_l + F2_l;
-			Fn_r = F1_r + F2_r;
-			if (((Fn_r) / (10000000000)) > 0)
-			{
-				Fn_l++;
-				Fn_r = Fn_r % (10000000000);
-			}
-			F1_l = F2_l;
-			F1_r = F2_r;
-			F2_l = Fn_l;
-			F2_r = Fn_r;
-		}
-		if (Fn_l == 0)
-		{
-			printf("%lu", Fn_r);
-		}
-		else
-		{
-			printf("%lu%lu", Fn_l, Fn_r);
-		}
-		if (i != 97)
-			printf(", ");
-		else
-			printf("\n");
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j_1 = j / 1000000000;
+	j_2 = j % 1000000000;
+	k_1 = k / 1000000000;
+	k_2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k_1 + (k_2 / 1000000000));
+		printf("%lu", k_2 % 1000000000);
+		k_1 = k_1 + j_1;
+		j_1 = k_1 - j_1;
+		k_2 = k_2 + j_2;
+		j_2 = k_2 - j_2;
+	}
+
+	printf("\n");
+
 	return (0);
 }
