4634d3c43b1af845a8e5fa0a8c9c57172b4fabc8
douniiia98 <donyabachar6@gmail.com>
2023-09-02 02:45:31 +0300

    argc,argv,lib

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,10 @@
  * Return: if success, returns 0.
 */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
